0de170893d851c091f2d56c4e355b35f52294a8b
Pratik Patel <ppatel@aecordigital.com>
2020-06-27 12:26:12 +0530

    First commit

---
+++ ImageFeed/ImageFeed/AppDelegate.swift
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.swift
+//  ImageFeed
+//
+//  Created by Pratik Patel on 27/06/20.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+
+        /// Sets FeedItemListViewController as rootViewController
+        if let keyWindow = window {
+            let feedItemListVC = FeedItemListViewController()
+            let rootViewController = UINavigationController(rootViewController: feedItemListVC)
+            keyWindow.rootViewController = rootViewController
+            keyWindow.makeKeyAndVisible()
+        }
+        return true
+    }
+}
+
---
+++ ImageFeed/ImageFeed/FeedItemListViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FeedItemListViewController.swift
+//  ImageFeed
+//
+//  Created by Pratik Patel on 27/06/20.
+//
+
+import UIKit
+
+class FeedItemListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        /// Sets views background color
+        self.view.backgroundColor = .white
+        /// Sets title in navigation bar
+        self.title = "Feed item list demo"
+    }
+}
